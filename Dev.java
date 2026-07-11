b43f4cd168efd54beab77b4c92cce19b7173d813
devnamdev2003 <devnamdevcse@gmail.com>
2026-07-11 19:09:55 +0530

    update

--- Dev.java
+++ Dev.java
@@ -1,13 +1,71 @@
 import java.util.*;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Dev {
+    public static TreeNode buildTree(Integer[] arr) {
+        if (arr == null || arr.length == 0 || arr[0] == null) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int i = 1;
+
+        while (!queue.isEmpty() && i < arr.length) {
+            TreeNode current = queue.poll();
+
+            // Left child
+            if (i < arr.length && arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.offer(current.left);
+            }
+            i++;
+
+            // Right child
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.offer(current.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         Solution o = new Solution();
+        Integer[] arr = {
+                1, 5, 3, null, 4, 10, 6, 9, 2
+        };
+
+        TreeNode root = buildTree(arr);
         System.out.println(o);
     }
 
 }
 
-class Solution {
 
+class Solution {
+    
 }
+
